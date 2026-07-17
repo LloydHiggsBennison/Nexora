9885de59c5b3e326ebae4e37f50cd8bf1f8e4002
unknown <lloydhiggs4@gmail.com>
2026-07-17 17:08:00 -0400

    update apiUrl for render

--- platform/src/environments/environment.ts
+++ platform/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   googleClientId: '869684077412-b8g8jsckpd646ump2tk3bil4ce2q04k5.apps.googleusercontent.com',
   adminEmail: '007.lloyd.higgs@gmail.com',
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://nexora-gsdu.onrender.com/api'
 };
